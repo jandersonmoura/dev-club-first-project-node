d9bbfbb81421c904a6fa3c06a4d4e5725908b440
jandersonmoura <janderson-moura@hotmail.com>
2022-09-30 20:47:03 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const uuid = require('uuid')
+
+
+const port = 3000
+const app = express()
+const users=[]
+app.use(express.json())
+
+const chackUserId = (request,response,next)=>{
+    const {id}=request.params
+    const index = users.findIndex(user => user.id === id)
+    if(index < 0){
+        return response.status(404).json({message:'Use Not Found'})
+    }
+    request.userIndex = index
+    request.userId = id
+
+    next()
+}
+
+app.get('/users',(request,response)=>{
+    return response.json(users)
+})
+
+app.post('/users',(request,response)=>{
+    const {name,age}=request.body
+    const user = {id:uuid.v4(),name,age}
+    users.push(user)
+    return response.status(201).json({user})
+})
+
+app.put('/users/:id', chackUserId,(request,response)=>{
+    
+    const {name,age} = request.body
+    const index = request.userIndex
+    const id = request.userId
+
+    const updateUser ={id,name,age}
+  
+    users[index] = updateUser
+    return response.json(updateUser)
+})
+
+app.delete('/users/:id', chackUserId,(request,response)=>{
+    const index = request.userIndex
+    users.splice(index,1)
+    return response.status(201).json()
+})
+
+
+app.listen(port,()=>{
+    console.log(`serve started on ${port}`)
+})
